1493e390167134581d5ef27ee2f5cd85ad9f0655
eugenehan94 <eugene.han94@gmail.com>
2023-11-13 15:04:54 -0500

    Spring boot quiz app

---
+++ src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -0,0 +1,35 @@
+package com.example.quizapp.controller;
+
+import com.example.quizapp.model.Question;
+import com.example.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestion() {
+
+        return questionService.getAllQuestions();
+    }
+
+    //    @GetMapping("category/{cat}") - since name of {cat} not same as String category - we have to specify in @PathVariable("cat")
+    //    public List<Question> getQuestionsByCategory(@PathVariable("cat") String category){
+    //    }
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+}
